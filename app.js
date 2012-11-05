b23163c28bcfe5f02f66d0478a81617bb0eaab15
Hortinstein <hortinstein@gmail.com>
2012-11-05 21:49:18 +0300

    changed name

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('coffee-script');
  */
 require('nodetime').profile({
     accountKey: '3a92a49d862b1674e0d0d0582b382dae1a21a81f', 
-    appName: 'Node.js Application'
+    appName: 'metaHypem'
   });
 
 var express = require('express')

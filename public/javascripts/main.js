c0c2565a2b933e7ea8b6c5fdd8112349030d3380
Hortinstein <hortinstein@gmail.com>
2012-11-07 22:19:50 +0300

    fixed it

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
 
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-36185226-1']);
@@ -9,5 +8,3 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-
-</script>

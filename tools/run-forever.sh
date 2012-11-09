a937eafacb004d2a43ecce4cc166d80d54bccff0
Farid Zakaria <farid.m.zakaria@gmail.com>
2012-11-09 11:43:45 -0800

    Added NODE_ENV production

--- tools/run-forever.sh
+++ tools/run-forever.sh
@@ -1,3 +1,3 @@
  #!/bin/bash    
 echo "Starting app.js as www-data user..."
-sudo -u www-data forever start -o /home/fzakaria/public/metahypem.com/log/access.log -e /home/fzakaria/public/metahypem.com/log/error.log node ../app.js
+NODE_ENV=production sudo -u www-data forever start -o /home/fzakaria/public/metahypem.com/log/access.log -e /home/fzakaria/public/metahypem.com/log/error.log node ../app.js

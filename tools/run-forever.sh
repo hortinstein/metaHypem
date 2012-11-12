575277722fa317f9ea760366bd99fc2de735dea5
Farid Zakaria <fzakaria@fzakaria.com>
2012-11-12 22:09:54 +0000

    removed run-forever script. Will define the script to use later

--- tools/run-forever.sh
+++
@@ -1,3 +0,0 @@
- #!/bin/bash    
-echo "Starting app.js as www-data user..."
-NODE_ENV=production sudo -u www-data forever start -o /home/fzakaria/public/metahypem.com/log/access.log -e /home/fzakaria/public/metahypem.com/log/error.log node ../app.js
